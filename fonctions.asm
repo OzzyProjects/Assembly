09b7b502f1d776a32b3389a59c869a0cae5e7112
ARMANGAU Etienne <81378159+OzzyProjects@users.noreply.github.com>
2021-03-26 12:07:21 +0100

    Add files via upload

--- fonctions.asm
+++ fonctions.asm
@@ -1,6 +1,58 @@
 ;------------------------------------------
-; equivalent de strlen() en C
-; fonction qui calcule la taille d'une string
+; fonction qui affiche un integer
+;------------------------------------------
+iprint:
+    push    eax             
+    push    ecx             
+    push    edx             
+    push    esi             
+    mov     ecx, 0          
+ 
+divideLoop:
+    inc     ecx             
+    mov     edx, 0          
+    mov     esi, 10         
+    idiv    esi             
+    add     edx, 48         
+    push    edx             
+    cmp     eax, 0          
+    jnz     divideLoop      
+ 
+printLoop:
+    dec     ecx             
+    mov     eax, esp        
+    call    sprint         
+    pop     eax            
+    cmp     ecx, 0         
+    jnz     printLoop      
+ 
+    pop     esi            
+    pop     edx            
+    pop     ecx            
+    pop     eax            
+    ret
+ 
+ 
+;------------------------------------------
+; fonction qui print un integer avec linefeed
+;------------------------------------------
+iprintLF:
+    call    iprint         
+ 
+    push    eax            
+    mov     eax, 0Ah       
+    push    eax            
+    mov     eax, esp       
+    call    sprint         
+    pop     eax            
+    pop     eax            
+    ret
+ 
+ 
+;------------------------------------------
+; fonction qui calcule la taille d'une chaine de caracteres
+; strlen() en C
+;------------------------------------------
 slen:
     push    ebx
     mov     ebx, eax
@@ -15,9 +67,11 @@ finished:
     sub     eax, ebx
     pop     ebx
     ret
+ 
+ 
 ;------------------------------------------
-; equivalent de printf
-; fonction qui affiche une string à l'ecran
+; fonction qui affiche une chaine de caracteres
+;------------------------------------------
 sprint:
     push    edx
     push    ecx
@@ -37,9 +91,27 @@ sprint:
     pop     ecx
     pop     edx
     ret
+ 
+ 
 ;------------------------------------------
-; void exit()
-; Permet de mettre fin au programme
+; fonction qui affiche une chaine de caracteres avec line feed
+;------------------------------------------
+sprintLF:
+    call    sprint
+ 
+    push    eax
+    mov     eax, 0AH
+    push    eax
+    mov     eax, esp
+    call    sprint
+    pop     eax
+    pop     eax
+    ret
+ 
+ 
+;------------------------------------------
+; fonction qui quitte le programme
+;------------------------------------------
 quit:
     mov     ebx, 0
     mov     eax, 1
