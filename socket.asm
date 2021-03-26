1cdf1c524a8432c0671b72eb550f5c98eab1c25e
ARMANGAU Etienne <81378159+OzzyProjects@users.noreply.github.com>
2021-03-26 13:34:37 +0100

    Update socket.asm

--- socket.asm
+++ socket.asm
@@ -1,5 +1,5 @@
 ; Socket écoutant sur le port 9001 en attente de connexions. Il affiche les headers de la requete lorsqu'un client se connecte et lui envoie en retour un
-Hello Wolrd
+; Hello Wolrd
 
 ; compilez avec nasm : nasm -f elf socket.asm
 ; linkez avec (64 bit systems require elf_i386 option): ld -m elf_i386 socket.o -o socket
